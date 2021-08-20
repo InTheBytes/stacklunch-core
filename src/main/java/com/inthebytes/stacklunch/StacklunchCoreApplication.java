a94e45a2baf09490893757e09ef86430ee11240f
Lexi Nelson <lexi.nelson@smoothstack.com>
2021-08-19 17:35:37 -0700

    Initial commit of bare spring-boot project

---
+++ src/main/java/com/inthebytes/stacklunch/StacklunchCoreApplication.java
@@ -0,0 +1,13 @@
+package com.inthebytes.stacklunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StacklunchCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StacklunchCoreApplication.class, args);
+	}
+
+}
