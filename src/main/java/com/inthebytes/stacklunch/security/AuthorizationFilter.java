ee0b5ee51a981ab90cbecebe8a9651b1df8ac28d
Lexi Nelson <lexi.nelson@smoothstack.com>
2021-08-27 12:43:09 -0700

    ITB-231: Removed dead import

--- src/main/java/com/inthebytes/stacklunch/security/AuthorizationFilter.java
+++ src/main/java/com/inthebytes/stacklunch/security/AuthorizationFilter.java
@@ -22,7 +22,6 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.inthebytes.stacklunch.data.authorization.AuthorizationRepository;
 
