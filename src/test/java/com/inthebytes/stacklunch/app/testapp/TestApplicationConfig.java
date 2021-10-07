2faa883d5a4dd638f3c7350a0268546ab3ac7a1e
Lexi Nelson <lexi.nelson@smoothstack.com>
2021-10-07 10:28:20 -0700

    Removed AWS imports (only used in account service)

--- src/test/java/com/inthebytes/stacklunch/app/testapp/TestApplicationConfig.java
+++ src/test/java/com/inthebytes/stacklunch/app/testapp/TestApplicationConfig.java
@@ -2,7 +2,6 @@ package com.inthebytes.stacklunch.app.testapp;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 
 import com.inthebytes.stacklunch.UniversalStackLunchConfiguration;
 
--- src/test/java/com/inthebytes/stacklunch/app/web/SecurityAndCorsTest.java
+++ src/test/java/com/inthebytes/stacklunch/app/web/SecurityAndCorsTest.java
@@ -1,6 +1,5 @@
 package com.inthebytes.stacklunch.app.web;
 
-import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
